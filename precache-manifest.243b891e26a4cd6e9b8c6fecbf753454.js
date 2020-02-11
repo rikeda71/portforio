3faa975ede76df3939704f7b99a27cd7f97ed503
Ryuya Ikeda <rikeda71@gmail.com>
2020-02-11 15:02:21 +0900

    Updates

--- precache-manifest.243b891e26a4cd6e9b8c6fecbf753454.js
+++ precache-manifest.243b891e26a4cd6e9b8c6fecbf753454.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "be51eea80d808d7fc56dc80422a7f090",
+    "revision": "771bc330ad79fa42b08f81df1fbf8c68",
     "url": "/portforio/index.html"
   },
   {
-    "revision": "4a8daac84f36274edfb9",
+    "revision": "677f192aac8a56ce942f",
     "url": "/portforio/static/css/main.d908d9ad.chunk.css"
   },
   {
-    "revision": "558d87bbba8d706d56bd",
-    "url": "/portforio/static/js/2.e83eb58e.chunk.js"
+    "revision": "c403819ae5cc7617062c",
+    "url": "/portforio/static/js/2.b0de179a.chunk.js"
   },
   {
-    "revision": "4a8daac84f36274edfb9",
-    "url": "/portforio/static/js/main.05b0c462.chunk.js"
+    "revision": "677f192aac8a56ce942f",
+    "url": "/portforio/static/js/main.17ab5622.chunk.js"
   },
   {
     "revision": "5f009cf24ac8f80b9240",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portforio/precache-manifest.aaea6a93186c11e57b18579407ef3ba6.js"
+  "/portforio/precache-manifest.243b891e26a4cd6e9b8c6fecbf753454.js"
 );
 
 self.addEventListener('message', (event) => {
